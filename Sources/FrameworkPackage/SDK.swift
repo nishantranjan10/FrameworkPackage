b584b6625edcbb520da08333edad227bec76ca81
Nishant Ranjan <nishantranjan10@gmail.com>
2025-12-18 09:23:52 +0800

    Initial commit package

---
+++ Sources/FrameworkPackage/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  FrameworkPackage
+//
+//  Created by Nishant Ranjan on 18/12/25.
+//
+
+import Foundation
+
+struct SDK {
+    static func doSomeWork() {
+        print("Doing some work!")
+    }
+}
